b7a6a43270225b6c175841b31944ccfe1f3e34f0
NastyaVasenina <amedeo_@mail.ru>
2020-09-30 21:50:12 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length==0){return 0}
+  return array.sort((a, b) => a - b )[0]
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length==0){return 0}
+  return array.sort((a, b) => b - a )[0]
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length==0){return 0}
+  return (array.sort((a, b) => a - b ).reduce(function(previousValue, currentValue) {
+    return previousValue + currentValue;
+  }))/array.length;
 }
